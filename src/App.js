c722cbcd5e186d4afe382d33ce49f03ef87f48bd
Galoopi <tiaanjvv@gmail.com>
2019-08-20 12:30:38 +0800

    change image link

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://safe-earth-78897.herokuapp.com:3000/image', {
+          fetch('https://safe-earth-78897.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
